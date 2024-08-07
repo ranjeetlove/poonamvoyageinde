f0bf21db0b479422e8aa33b7b210e4ef32f77fad
sunilgupta-green-coding <sunilgupta.csm2@gmail.com>
2024-08-07 16:35:25 +0530

    git push

---
+++ app/Models/Rating.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tour_id',
+        'user_id',
+        'rating',
+    ];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
