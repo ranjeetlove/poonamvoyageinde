a4731d8f047d0570bbadd35a4b553baae81cf2f5
sunilgupta-green-coding <sunilgupta.csm2@gmail.com>
2024-08-08 22:33:47 +0530

    git push

--- resources/views/frontend/blog-details.blade.php
+++ resources/views/frontend/blog-details.blade.php
@@ -10,36 +10,39 @@
                </ul>
             </div>
          </div>
-		 
+
          <div class="bg-image">
             <img src="{{asset('uploads/banners/'.$banner->image)}}" alt="Demo Image">
          </div>
       </div>
-	  
-	  
-	  
+
+
+
 
            <section class="blog-details-section pt-100 pb-70">
          <div class="container">
-         
+
             <div class="row">
                <div class="col-lg-8 col-md-12">
                   <div class="blog-details-desc mb-30">
                      <div class="image mb-20">
-                        <img src="{{asset('/uploads/blog/'.$blog->image)}}" alt="image" />
+                        {{-- <img src="{{asset('/uploads/blog/'.$blog->image)}}" alt="image" /> --}}
+                        <img src="{{ isset($blog) && $blog->image && file_exists(public_path('uploads/blog/'.$blog->image)) ?
+                        asset('uploads/blog/'.$blog->image) : asset('uploads/tour/image/no-image.jpg') }}"
+                        alt="">
                      </div>
                      <ul class="info-list mb-20">
-                        <li><i class='bx bx-calendar'></i> {{$blog->posted_date}}</li>
-                        <li><i class='bx bx-tag'></i>{{$blog->category}}</li>
+                        <li><i class='bx bx-calendar'></i> {{ !empty($blog->posted_by) ? $blog->posted_by : '' }}</li>
+                        <li><i class='bx bx-tag'></i>{{ !empty($blog->category) ? $blog->category : '' }}</li>
                      </ul>
                      <div class="content mb-20">
-                        <h3>{{$blog->title}}</h3>
-                        <p>{!!$blog->content!!}</p>						
+                        <h3>{{ !empty($blog->title) ? $blog->title : '' }}</h3>
+                        <p>{!! !empty($blog->content) ? $blog->content : '' !!}</p>
                      </div>
                   </div>
                </div>
-               
-               
+
+
                <div class="col-lg-4 col-md-12">
                   <aside class="widget-area">
                      <div class="widget widget-article mb-30">
@@ -51,20 +54,20 @@
                            </div>
                            <div class="content">
                               <h3>
-                                 <a href="{{route('blogdetails',$blog->slug) }}">{{$blog->title}}</a>
+                                 <a href="{{route('blogdetails',$blog->slug) }}">{{ !empty($blog->title) ? $blog->title : '' }}</a>
                               </h3>
                               <ul class="list">
                                  <li>
                                     <div class="author">
                                        <img src="{{asset('frontend/assets/img/blog/author1.jpg')}}" alt="Demo Image">
-                                       <span>By - {{$blog->posted_by}}</span>
+                                       <span>By - {{ !empty($blog->posted_by) ? $blog->posted_by : '' }}</span>
                                     </div>
                                  </li>
                               </ul>
                            </div>
                         </article>
                         @endforeach
-                        					
+
 						{{-- <article class="article-item">
                            <div class="image">
                               <img src="assets/img/blogs/blog3.jpg" alt="Demo Image" />
@@ -83,7 +86,7 @@
                               </ul>
                            </div>
                         </article>
-						
+
 					  <article class="article-item">
                            <div class="image">
                               <img src="assets/img/blogs/blog4.jpg" alt="Demo Image" />
@@ -102,10 +105,10 @@
                               </ul>
                            </div>
                         </article> --}}
-           
-          
+
+
                      </div>
-            
+
                   </aside>
                </div>
             </div>
